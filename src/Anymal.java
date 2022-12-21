bdfd8bd7597552d9201945e03e216516e0ad1695
aizirek <aizirek.karimbaeva@gmail.com>
2022-12-21 17:45:12 +0300

    commit

---
+++ src/Anymal.java
@@ -0,0 +1,13 @@
+public class Anymal {
+    static int age;
+      public void setEmes(int age){
+          this.age = age;
+       }
+      public int getAge(){
+          return age;
+      }
+
+
+
+
+}
